1ffb22e2a6fd28b216afe3e550b40db2e0c74107
munkhtulga1 <munkhtulga@mirai.mn>
2023-11-23 12:27:23 +0800

    build fixed

--- app/_components/layouts/DesktopLayout.tsx
+++ app/_components/layouts/DesktopLayout.tsx
@@ -11,7 +11,7 @@ const DesktopLayout = () => {
     >
       <section className="tw-flex tw-flex-col md:tw-px-[25px] xl:tw-px-[50px]">
         <MainHeader />
-        <MainVideoFrame />
+        {/* <MainVideoFrame /> */}
       </section>
       <section className="tw-grid tw-auto-rows-min tw-place-content-center md:tw-px-[50px] xl:tw-px-[157.5px]">
         <MainDesktopOptions />
