073cd6245dd54fb3a2b743c60cfd5cdab4f4bb80
ariunbold@mirai.mn <ariunbold@mirai.mn>
2023-11-23 21:40:46 -0500

    updated reservation btn

--- app/_components/buttons/ButtonSecondary.tsx
+++ app/_components/buttons/ButtonSecondary.tsx
@@ -1,10 +1,12 @@
 const ButtonSecondary = () => {
     return (
-        <button className="tw-bg-primary tw-rounded-[50px] tw-py-4 tw-text-center tw-w-full">
-            <span className="tw-text-white tw-text-base">
-                ご予約はこちら
-            </span>
-        </button>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfz-4StBw917Gy9THky3US6b18naIPmUP3lEwxZ4FvPBTOSDA/viewform?usp=send_form">
+            <button className="tw-bg-primary tw-rounded-[50px] tw-py-4 tw-text-center tw-w-full">
+                <span className="tw-text-white tw-text-base">
+                    ご予約はこちら
+                </span>
+            </button>
+        </a>
     );
 }
 
