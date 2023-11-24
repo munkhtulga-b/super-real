e70b13ba61110b5e3cef0348db3d120aeab140f6
ariunbold@mirai.mn <ariunbold@mirai.mn>
2023-11-23 21:47:08 -0500

    updated target="_blank"

--- app/_components/buttons/ButtonSecondary.tsx
+++ app/_components/buttons/ButtonSecondary.tsx
@@ -1,6 +1,6 @@
 const ButtonSecondary = () => {
     return (
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfz-4StBw917Gy9THky3US6b18naIPmUP3lEwxZ4FvPBTOSDA/viewform?usp=send_form">
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfz-4StBw917Gy9THky3US6b18naIPmUP3lEwxZ4FvPBTOSDA/viewform?usp=send_form" target="_blank">
             <button className="tw-bg-primary tw-rounded-[50px] tw-py-4 tw-text-center tw-w-full">
                 <span className="tw-text-white tw-text-base">
                     ご予約はこちら
