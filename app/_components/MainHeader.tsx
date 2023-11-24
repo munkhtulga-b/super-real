28240bc49dc1af878a24567895b54e736e5948c7
ariunbold@mirai.mn <ariunbold@mirai.mn>
2023-11-23 20:54:01 -0500

    updated back-arrow & reservation btn

--- app/_components/MainHeader.tsx
+++ app/_components/MainHeader.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const MainHeader = () => {
   return (
-    <div className="tw-mt-[39px] tw-px-[28px] md:tw-px-0 tw-py-2 tw-flex tw-justify-center md:tw-justify-start tw-items-center tw-gap-x-4">
+    <div className="tw-mt-[39px] tw-px-[28px] md:tw-px-0 tw-py-2 tw-flex tw-justify-center md:tw-justify-start tw-gap-x-4">
       <button className="tw-w-[30px] tw-h-[30px] tw-grid tw-place-items-center">
         <Image
           src="/assets/return-button.svg"
--- app/_components/buttons/ButtonSecondary.tsx
+++ app/_components/buttons/ButtonSecondary.tsx
@@ -2,10 +2,10 @@ const ButtonSecondary = () => {
     return (
         <button className="tw-bg-primary tw-rounded-[50px] tw-py-4 tw-text-center tw-w-full">
             <span className="tw-text-white tw-text-base">
-                予約と金額
+                ご予約はこちら
             </span>
         </button>
     );
 }
- 
+
 export default ButtonSecondary;
