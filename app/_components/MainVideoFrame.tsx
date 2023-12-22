a2c7af9a16f98cd93cae0991b7a139292a14bd98
munkhtulga1 <munkhtulga@mirai.mn>
2023-12-22 15:14:19 +0800

    test 10

--- app/_components/MainVideoFrame.tsx
+++ app/_components/MainVideoFrame.tsx
@@ -30,7 +30,7 @@ const MainVideoFrame: React.FunctionComponent<VideoFrameProps> = ({
   current,
   onVideoEnd,
 }) => {
-  const [videoKey, setVideoKey] = useState(0);
+  const [muted, setMuted] = useState(true);
   const [canPlay, setCanPlay] = useState(false);
   const screenSize = useAppSelector((state) => state.store.screenSize);
   const playerRef = useRef<HTMLVideoElement | null>(null);
@@ -56,10 +56,7 @@ const MainVideoFrame: React.FunctionComponent<VideoFrameProps> = ({
 
   useEffect(() => {
     if (canPlay) {
-      setTimeout(() => {
-        const player: HTMLVideoElement = document.querySelector("#player")!;
-        player.muted = false;
-      }, 1000);
+      console.log("can play");
     }
   }, [canPlay]);
 
@@ -84,6 +81,44 @@ const MainVideoFrame: React.FunctionComponent<VideoFrameProps> = ({
 
   return (
     <div className="tw-mt-[50px] tw-w-full tw-relative">
+      <button
+        className="tw-absolute tw-top-4 tw-left-4 tw-z-30"
+        onClick={() => setMuted(!muted)}
+      >
+        {muted && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="tw-w-6 tw-h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+            />
+          </svg>
+        )}
+        {!muted && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="tw-w-6 tw-h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+            />
+          </svg>
+        )}
+      </button>
+
       <span
         style={{ writingMode: "vertical-rl", textOrientation: "upright" }}
         className={`${
@@ -134,7 +169,7 @@ const MainVideoFrame: React.FunctionComponent<VideoFrameProps> = ({
           playerRef={playerRef}
           preload="auto"
           autoPlay={true}
-          muted={true}
+          muted={muted}
           loop={!videoURL}
           src={
             !videoURL
